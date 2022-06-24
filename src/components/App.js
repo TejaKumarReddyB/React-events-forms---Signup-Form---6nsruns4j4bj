1dd2c6ed6528cc2a2dc705c06b01b39da2b9e370
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-06-25 00:13:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
-const App = () => {
+import './styles/App.css';
+import React from 'react';
+import { SignIn } from './pages/signin';
+import { SignUp } from './pages/signup';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
   return (
-    <div id="main"></div>
-  )
+    <Switch>
+      <Route exact path="/" component={SignIn} />
+      <Route path="/signin/" component={SignIn} />
+      <Route path="/signup/" component={SignUp} />
+    </Switch>
+  );
 }
-
 
 export default App;
