065fef7b07b1f5007579dfd3f263df1afd485d00
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-06-26 20:06:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,155 +1,139 @@
-import "./styles.css";
-import { signUpFormValidation } from "./validation";
-import React,{ useState } from "react";
-
-
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
 const App = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [gender, setGender] = useState("male");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [errors, setErrors] = useState("");
-  const obj = {
-    name: name,
-    email: email,
-    password: password,
-    gender: gender,
-    phoneNumber: phoneNumber
-  };
+  const [message, setMessage] = useState("Please fill all the details");
 
-  const handleClick = (ev) => {
-    ev.preventDefault();
-    const eve = signUpFormValidation(obj);
-    setErrors(eve);
-    if (eve === null) {
-      const inde = obj.email.indexOf("@");
-      const userName = "Hello " + obj.email.slice(0, inde);
-      setErrors(null);
-      setUser(userName);
-    } else {
-      setUser("");
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [genderError, setGenderError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleNameChange = (event) => {
+      setName(event.target.value);
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+  }
+
+  const handlePhoneChange = (event) => {
+    setPhoneNumber(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Clearing all the previous error messages
+    // Setting all the error messages to initial state
+    setMessage("Please fill all the details");
+    setNameError("");
+    setEmailError("");
+    setGenderError("");
+    setPhoneError("");
+    setPasswordError("");
+
+    let flag = true;
+
+    if(name == "" || email == "" || password == "" || phoneNumber == "") {
+      setMessage("All fields are mandatory");
+      flag = false;
     }
-  };
 
-  const handleChange = (e) => {
-    e.preventDefault();
-
-    if (e.target.id === "name") {
-      setName(e.target.value);
-      obj.name = e.target.value;
-    } else if (e.target.id === "email") {
-      setEmail(e.target.value);
-      obj.email = e.target.value;
-    } else if (e.target.id === "password") {
-      setPassword(e.target.value);
-      obj.password = e.target.value;
-    } else if (e.target.id === "gender") {
-      setGender(e.target.value);
-      obj.gender = e.target.value;
-    } else if (e.target.id === "phoneNumber") {
-      setPhoneNumber(e.target.value);
-      obj.phoneNumber = e.target.value;
+    if(name != "" && name.match(/((^[0-9]+[a-z]+)|(^[a-z]+[0-9]+))+[0-9a-z]+$/i) == null) {
+      setNameError("Name is not alphanumeric");
+      flag = false;
     }
-    //handleClick(e);
-  };
+
+    if(password != "" && password.length <= 5 ) {
+      setPasswordError("Password must contain atleast 6 letters");
+      flag = false;
+    }
+
+    if(email != "" && email.indexOf("@") == -1) {
+      setEmailError("Email must contain @");
+      flag = false;
+    }
+
+    let isNum = /^\d+$/.test(phoneNumber);
+
+    if(phoneNumber != "" && (!isNum)) {
+      setPhoneError("Phone Number must contain only numbers");
+      flag = false;
+    }
+
+    
+    if (flag) {
+      let username;
+      const pos = email.indexOf("@");
+      username = email.substring(0, pos);
+      setMessage(`Hello ${username}`);
+    }
+    
+  }
+
   return (
-    <>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          data-testid="name"
-          onChange={handleChange}
-        />
-        <br />
-        {errors !== null && errors.name !== "" ? (
-          <span>{errors.name}</span>
-        ) : (
-          ""
-        )}
-        <br />
-        <br />
-        <label htmlFor="email">Email: </label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          data-testid="email"
-          onChange={handleChange}
-        />
-        <br />
-        {errors !== null && errors.email !== "" ? (
-          <span>{errors.email}</span>
-        ) : (
-          ""
-        )}
-        <br />
-        <br />
-        <label htmlFor="gender">Gender: </label>
-        <select
-          data-testid="gender"
-          value={gender}
-          onChange={handleChange}
-          id="gender"
-        >
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="other">other</option>
-        </select>
-        <br />
-        {errors !== null && errors.gender !== "" ? (
-          <span>{errors.gender}</span>
-        ) : (
-          ""
-        )}
+    <div id="main">
+      <form className="container" onSubmit={handleSubmit}>
+        <h3>{message}</h3>
 
-        <br />
-        <br />
+        <div className="grid-item">
+          <label>Name</label>
+          <input type="text" data-testid = 'name' value={name} onChange={handleNameChange}/>
+          <h3>{nameError}</h3>
+        </div>
 
-        <label htmlFor="password">Password: </label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          data-testid="password"
-          onChange={handleChange}
-        />
-        <br />
-        {errors !== null && errors.password !== "" ? (
-          <h3>{errors.password}</h3>
-        ) : (
-          ""
-        )}
-        <br />
-        <br />
-        <label htmlFor="phoneNumber">Phone Number: </label>
-        <input
-          type="text"
-          id="phoneNumber"
-          value={phoneNumber}
-          onChange={handleChange}
-          data-testid="phoneNumber"
-        />
-        <br />
-        {errors !== null && errors.phoneNumber !== "" ? (
-          <h4>{errors.phoneNumber}</h4>
-        ) : (
-          ""
-        )}
-        <br />
-        <br />
-        <button type="submit" onClick={handleClick} data-testid="submit">
-          Signup
-        </button>
+        <div className="grid-item">
+          <label>Email address</label>
+          <input type="email" data-testid = 'email' value={email} onChange={handleEmailChange}/>
+          <h3>{emailError}</h3>
+        </div>
+
+        <div className="grid-item">
+          <label>Gender</label>
+          <select data-testid='gender' value={gender} onChange={handleGenderChange}>
+            <option value="male">male</option>
+            <option value="female">female</option>
+            <option value="others">others</option>
+          </select>
+          <h3>{genderError}</h3>
+        </div>
+
+        <div className="grid-item">
+          <label>Phone Number</label>
+          <input type="tel" data-testid = 'phoneNumber' value={phoneNumber} onChange={handlePhoneChange}/>
+          <h3>{phoneError}</h3>
+        </div>
+
+        <div className="grid-item">
+          <label>Password</label>
+          <input type="password" data-testid = 'password' value={password} onChange={handlePasswordChange}/>
+          <h3>{passwordError}</h3>
+        </div>
+
+        <div className="grid-item">
+          <button type="submit" data-testid = 'submit'>Submit</button>
+        </div>
+        
       </form>
-      {user !== "" ? <p>{user}</p> : ""}
-    </>
-  );
-};
+    </div>
+  )
+}
+
+
 export default App;
